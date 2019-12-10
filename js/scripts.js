6a512161dd276ef4ab4ae4a0215f15d946dac6f8
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-12-09 19:44:43 -0600

    add JS for working application, including random inputs that won't work with the loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
 
-	// Back-End (business logic)
+// Back-End (business logic)
 
-	var total = 0;
-	const countedNumbers = [];
-	
+var total = 0;
+var answers = [];
 
-	
-	// Front-End (user-interface logic)
-	
-	$(document).ready(function() {
-		$("form#calculator").submit(function() {
-			event.preventDefault();
-			var goalNumber = parseInt($("#goalInput").val());
-			var countingNumber = parseInt($("#countInput").val());
-			console.log("goal number: " + goalNumber);  // for debugging
-    	console.log("counting number: " + countingNumber);  // for debugging
+// Front-End (user-interface logic)
 
-			//if else for empty and non-number inputs
+$(document).ready(function () {
+	$("form#calculator").submit(function () {
+		event.preventDefault();
+		var goalNumber = parseInt($("#goalInput").val());
+		var countingNumber = parseInt($("#countInput").val());
 
+		//if else for empty and non-number inputs
+		if (isNaN(goalNumber) || isNaN(countingNumber)) {
+			$("#output").text("Sorry, I only know how to count by numbers!");
+		} else if (goalNumber <= countingNumber) {
+			$("#output").text("Please make your goal number larger than your counting number.");
+		} else if (countingNumber <= 0) {
+			$("#output").text("I'd rather not crash from an infinite loop. Try again.");
+		} else {
 			//for loop to add counting number until goal number
 			for (var currentNumber = 0; currentNumber < goalNumber; currentNumber += countingNumber) {
-				console.log(currentNumber);
-				var answers = countedNumbers.push(currentNumber);
-				//countedNumbers.push([currentNumber]);
+				answers.push(currentNumber);
 			}
-			 
-				console.log(answers);
-			$("#output").text(answers);
-		})
+
+			$("#output").text(answers + "," + goalNumber);
+		}
 	})
+})
